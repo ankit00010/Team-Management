22bc05f4ffed9798db7ddedbcbf26d200646cb97
ankit00010 <111192702+ankit00010@users.noreply.github.com>
2023-12-03 00:13:00 +0530

    Search minor issue solved

--- client/user-management-app/src/PageLayout.jsx
+++ client/user-management-app/src/PageLayout.jsx
@@ -1,42 +1,31 @@
 // PageLayout.js
 import React, { useState, useEffect } from 'react';
-import Search from './components/Search';
-import Filter from './components//Filter';
-import Footer from './components//Footer';
-import UserList from './components//UserList';
-import TeamCreation from './components//TeamCreation';
+import UserSearch from './components/Search';
+import Filter from './components/Filter';
+import Footer from './components/Footer';
+import UserList from './components/UserList';
+import TeamCreation from './components/TeamCreation';
 
 const PageLayout = () => {
     const [searchResults, setSearchResults] = useState([]);
     const [filteredUsers, setFilteredUsers] = useState([]);
 
-    const handleAddToTeam = (userId, isAdded) => {
-        console.log('Adding/removing user from team (PageLayout):', userId, isAdded);
-        // Implement the logic to add/remove users from the team
-    };
-
-    const handleSearch = async (searchTerm) => {
-        try {
-            const response = await fetch(`http://localhost:5000/api/users/search/${searchTerm}`);
-            const userData = await response.json();
-            setSearchResults(userData.data);
-        } catch (error) {
-            console.error('Error searching users:', error);
-            setSearchResults([]);
-        }
+    const handleSearch = (userData) => {
+        setSearchResults(userData);
     };
 
     const handleFilterChange = async (filterOptions) => {
         try {
-            const { domain, gender, available } = filterOptions;
-            const url = `http://localhost:5000/api/users/filter?domain=${domain || ''}&gender=${gender || ''}&available=${available || ''}&page=1`;
-            const response = await fetch(url);
-            const data = await response.json();
-            setFilteredUsers(data);
+            // ... (same as before)
         } catch (error) {
             console.error('Error fetching filtered users:', error);
             setFilteredUsers([]);
         }
+    };
+
+    const handleAddToTeam = (userId, isAdded) => {
+        console.log('Adding/removing user from team (PageLayout):', userId, isAdded);
+        // Implement the logic to add/remove users from the team
     };
 
     useEffect(() => {
@@ -45,9 +34,9 @@ const PageLayout = () => {
 
     return (
         <div>
-            <Search onSearch={handleSearch} />
+            <UserSearch onSearch={handleSearch} />
             <Filter onFilterChange={handleFilterChange} />
-            <TeamCreation onCreateTeam={() => { }} onAddToTeam={handleAddToTeam} />
+            <TeamCreation onCreateTeam={() => { }} />
             <UserList
                 searchResults={searchResults}
                 filteredUsers={filteredUsers}
