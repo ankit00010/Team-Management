d5624b9dd4cb872daa35b7065f29d26372f57dfb
ankit00010 <111192702+ankit00010@users.noreply.github.com>
2023-12-04 23:09:58 +0530

    added cors origin

--- server/server.js
+++ server/server.js
@@ -8,22 +8,19 @@ dotenv.config();
 connectDB();
 const app = express();
 app.use(cors({
-
-    origin: ["http"],
+    origin: ["https://user-management-api-two.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true
-
-
 }));
 
-const port = process.env.PORT || 5001; // Use port 5001 as a default
-
+const port = process.env.PORT || 5001;
 
 app.use(express.json());
 
 app.use('/api/users', require('./router/userRoutes'));
 app.use("/api/team", require("./router/teamRouter"));
 app.use(errorHandler);
+
 app.listen(port, () => {
     console.log(`The server is running on port ${port}`);
 });
