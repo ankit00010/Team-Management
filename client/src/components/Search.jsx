5714e9af92e25499b95e1210f82e406b50330df3
ankit00010 <111192702+ankit00010@users.noreply.github.com>
2023-12-05 13:41:16 +0530

    Update Search.jsx

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -14,7 +14,7 @@ const Search = ({ onSearch }) => {
 
             onSearch(userData.data);
         } catch (error) {
-            console.error('Error searching users:', error);
+         
             onSearch([]);
         }
     };
