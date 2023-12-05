89c9787a80677e943a7a72e2453bad9ff96614bd
ankit00010 <111192702+ankit00010@users.noreply.github.com>
2023-12-05 13:52:52 +0530

    Update UserList.jsx

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -2,28 +2,38 @@ import React, { useEffect, useState } from 'react';
 import UserCard from './UserCard';
 
 const UserList = ({ searchResults, filteredUsers, onToggleSelect }) => {
+    // State to manage the list of users
     const [userList, setUserList] = useState([]);
 
+    // Effect to fetch data when the component mounts or searchResults/filteredUsers change
     useEffect(() => {
         fetchData();
     }, [searchResults, filteredUsers]);
 
+    // Function to fetch data based on search results or filtered users
     const fetchData = async () => {
         try {
+            // Check if search results are available and update the user list accordingly
             if (searchResults && searchResults.length > 0) {
                 setUserList(searchResults);
-            } else if (filteredUsers && filteredUsers.length > 0) {
+            }
+            // Check if filtered users are available and update the user list accordingly
+            else if (filteredUsers && filteredUsers.length > 0) {
                 setUserList(filteredUsers);
-            } else {
+            }
+            // Fetch all users if neither search results nor filtered users are available
+            else {
                 const response = await fetch('https://user-management-api-eight.vercel.app/api/users');
                 const data = await response.json();
                 setUserList(data);
             }
         } catch (error) {
+            // Handle errors when fetching data
             console.error('Error fetching data:', error);
         }
     };
 
+    // Render the list of users using UserCard component
     return (
         <div className="user-list">
             {userList.map((user) => (
