645a104acd568e5d4762be38a2a965bfa12ceff5
ankit00010 <111192702+ankit00010@users.noreply.github.com>
2023-12-05 19:48:37 +0530

    delete feature

--- client/src/components/TeamListItem.jsx
+++ client/src/components/TeamListItem.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './TeamListItem.css';
-
+import '../../public/styles/main.css'
 const TeamListItem = ({ teamId }) => {
     const [team, setTeam] = useState(null);
 
